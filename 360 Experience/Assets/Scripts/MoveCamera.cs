71f2a27de290d9c7eeb65253a5ff0f697e23dcd7
Fatih Bagcaci <fatih.bagcaci@hva.nl>
2020-09-23 18:39:00 +0200

    Made a prefab for the 360 videoplayer. The 360 videoplayer plays the video selected in the main menu. The user can move their phone to look around.

---
+++ 360 Experience/Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    private bool gyroExists;
+    private Gyroscope gyro;
+    private Quaternion rotation;
+
+    void Start()
+    {
+        gyroExists = CheckGyro();
+
+    }
+
+    void Update()
+    {
+        // Rotate camera with phone.
+        if(gyroExists){
+            transform.localRotation = gyro.attitude * rotation;
+        }
+    }
+
+    // Check if phone has a built in gyroscope.
+    private bool CheckGyro(){
+        // The phone has a built in gyroscope.
+        if(SystemInfo.supportsGyroscope){
+            // Set the gyroscope to the phone's built in gyroscope and enable it.
+            gyro = Input.gyro;
+            gyro.enabled = true;
+
+            // Setup for camera movement with phone.
+            transform.parent.rotation = Quaternion.Euler(90f, 280f, 0f);    // Allign 360 video orientation to real world orientation. 
+            rotation = new Quaternion(0, 0, 1, 0);                          // Revert inverted camera rotation.
+
+            return true;
+        }
+
+        // The phone has no built in gyroscope.
+        return false;
+    }
+}
---
+++ 360 Experience/Assets/Scripts/VideoPlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoPlayerController : MonoBehaviour
+{
+
+    // Play the video selected in the main menu.
+    void Start()
+    {
+        var videoPlayer = gameObject.GetComponent<UnityEngine.Video.VideoPlayer>();
+        VideoClip videoClip = Resources.Load<VideoClip>(VideoSelection.SetVideoClip()) as VideoClip;
+        videoPlayer.clip = videoClip;
+        videoPlayer.Play();
+    }
+}

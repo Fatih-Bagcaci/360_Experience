8cda03653b521ebeaf5276ab6add72939bacc179
Fatih Bagcaci <fatih.bagcaci@hva.nl>
2020-09-24 22:09:38 +0200

    The home button in video playback menu now returns user to the main menu.

---
+++ 360 Experience/Assets/Scripts/ReturnToMainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReturnToMainMenu : MonoBehaviour
+{
+    private Button button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(GoToMainMenu);
+    }
+
+    private void GoToMainMenu(){
+        SceneController.GoToScene(SceneController.Scene.MainMenu);
+    }
+}
